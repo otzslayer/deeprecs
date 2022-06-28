5514718a8c34e8ad36e677a092fbc4efe6f573d0
Jay Han <otzslayer@gmail.com>
2022-06-29 08:58:46 +0900

    style: 추천 베이스 클래스
    - 추천 베이스 클래스 `BaseRecommender` 공유

---
+++ deeprecs/models/base.py
@@ -0,0 +1,37 @@
+from abc import ABCMeta, abstractmethod
+
+import torch.nn as nn
+
+
+class BaseRecommender(nn.Module):
+    r"""추천 모델을 위한 베이스 클래스입니다.
+    모든 모델은 이 클래스의 서브클래스여야 합니다.
+
+    이 베이스 클래스를 상속받는 모든 클래스는 다음 메서드를 포함하고 있습니다.
+
+    `forward`
+        : Forward propagation을 하는 메서드
+    `fit`
+        : 모델을 데이터에 적합시키는 메서드로 `_train_one_epoch`을 반복하게 됩니다.
+    `predict`
+        : 추천 결과를 생성하는 메서드
+    `_train_one_epoch`
+        : 1 epoch을 학습하는 메서드
+    """
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def forward(self):
+        pass
+    
+    @abstractmethod
+    def fit(self):
+        pass
+
+    @abstractmethod
+    def predict(self):
+        pass
+
+    @abstractmethod
+    def _train_one_epoch(self):
+        pass
