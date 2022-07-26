8ad29b573386cb1477cb551af20ef880b89e2fb5
yrc726 <yyyura@naver.com>
2022-07-26 10:00:27 +0900

    add docstring, refine init function

--- deeprecs/models/CDAE.py
+++ deeprecs/models/CDAE.py
@@ -6,36 +6,44 @@ from deeprecs.models.base import BaseRecommender
 
 
 class CDAE(BaseRecommender):
-    r"""CDAE model docstring
-    Args:
-        BaseRecommender (_type_): _description_
+    r"""CDAE(Collaborative Denoising AutoEncoder) 추천 모델
+
+    Parameters
+    ----------
+    num_users : int
+        사용자 수
+    num_items : int
+        아이템 수
+    hidden_dim : int
+        은닉층 차원
+    hparams : dictionary
+        hyper-parameter들을 담은 dictionary
+    References
+    ----------
+    [1] Wu, Yao, et al. "Collaborative Denoising Auto-Encoders for Top-N Recommender Systems."
+        Proceedings of the 9th ACM International Conference on Web Search and Data Mining. 2016.
+        https://alicezheng.org/papers/wsdm16-cdae.pdf
     """
 
-    def __init__(
-        self,
-        num_users,
-        num_items,
-        latent_dim,
-        hparams,
-        is_train,
-    ):
+    def __init__(self, num_users, num_items, hidden_dim, hparams):
         super().__init__()
-        self.corruption_ratio = hparams["corruption_ratio"]
+
         self.num_users = num_users
         self.num_items = num_items
-        # self.encoder_dims = encoder_dims
-        self.latent_dim = latent_dim
+        self.corruption_ratio = hparams["corruption_ratio"]
+        self.hidden_dim = hidden_dim
+
         self.user_embedding = nn.Embedding(
             num_embeddings=self.num_users,  # embedding할 노드 수
-            embedding_dim=self.latent_dim,
+            embedding_dim=self.hidden_dim,
         )  # embedding할 벡터 차원
-        self.encoder = nn.Linear(self.num_items, self.latent_dim)
-        self.decoder = nn.Linear(self.latent_dim, self.num_items)
+        self.encoder = nn.Linear(self.num_items, self.hidden_dim)
+        self.decoder = nn.Linear(self.hidden_dim, self.num_items)
+
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu"
         )
         self.to(self.device)
-        self.is_train = is_train
 
     def forward(self, item_vector, user_vector):
         print("forward")
