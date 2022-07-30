08fdbd185d28b79308851febc096371b8ac75177
kwonsk <nerochaos1@gmail.com>
2022-07-30 14:25:43 +0900

    feat: AutoRec의 fit/predict 함수 작성
    - AutoRec의 fit/predict 함수 작성
    - _train_one_epoch 작성
    - 생성자에 loss랑 optimizer 추가
    - 기존에 부족했던 docstring 완성

--- deeprecs/models/autorec.py
+++ deeprecs/models/autorec.py
@@ -1,6 +1,11 @@
 from typing import Final, Union
 
+import torch
 from torch import nn
+from torch.nn.modules.loss import _Loss
+from torch.optim import Adam
+from torch.optim.optimizer import Optimizer
+from torch.utils.data import DataLoader, Dataset
 
 from deeprecs.models.base import BaseRecommender
 from deeprecs.utils.helper import get_classes_from_module
@@ -36,7 +41,7 @@ class AutoEncoder(BaseRecommender):
 
     References
     ----------
-    https://users.cecs.anu.edu.au/~akmenon/papers/autorec/autorec-paper.pdf
+    [1] https://users.cecs.anu.edu.au/~akmenon/papers/autorec/autorec-paper.pdf
     """
 
     def __init__(
@@ -44,6 +49,8 @@ class AutoEncoder(BaseRecommender):
         input_dim: int,
         hidden_dim: int,
         final_activation: ACTIVATION = nn.ReLU(),
+        optimizer: Union[str, Optimizer] = "adam",
+        loss: _Loss = nn.MSELoss(),
     ):
         super().__init__()
         self._input_dim = input_dim
@@ -51,20 +58,49 @@ class AutoEncoder(BaseRecommender):
         self._encoder = nn.Linear(input_dim, hidden_dim)
         self._decoder = nn.Linear(hidden_dim, input_dim)
         self._final_activation = final_activation
+        if isinstance(optimizer, str):
+            self._set_optimizer(optimizer)
+        elif isinstance(optimizer, Optimizer):
+            self._optimizer = optimizer
+        else:
+            raise ValueError
+        self._loss = loss
 
-    def forward(self, input_data):
+    def _set_optimzer(self, optimizer: str):
         """
-        _summary_
+        optimizer 이름을 실제 optimizer로 변경
 
         Parameters
         ----------
-        input_data : _type_
-            _description_
+        optimizer : str
+            optimizer 이름
+
+        Raises
+        ------
+        ValueError
+            미리 정해지지 않은 optimizer 이름을 사용할 경우 에러
+        """
+        optimizer = optimizer.lower()
+        if optimizer == "adam":
+            self._optimizer = Adam(self.parameters(), lr=0.001)
+        else:
+            raise ValueError
+
+    def forward(self, input_data: torch.Tensor) -> torch.Tensor:
+        """
+        Forward propagation을 수행합니다.
+
+        Parameters
+        ----------
+        input_data : torch.Tensor
+            (n_samples, n_input_dim)의 형태.
+            Forward propagation을 수행할 input samples.
 
         Returns
         -------
-        _type_
-            _description_
+        torch.Tensor
+            (n_samples, n_input_dim)의 형태.
+            Forward propagation 수행이 끝난 후의 결과
         """
         encoded = self._encoder(input_data)
         decoded = self._decoder(encoded)
@@ -72,135 +108,213 @@ class AutoEncoder(BaseRecommender):
 
         return output
 
-    def fit(self):
+    def fit(self, trainset: Dataset, epochs: int, batch_size: int):
         """
-        _summary_
+        모델을 데이터에 적합시키는 메서드로 `_train_one_epoch`을 반복합니다.
 
-        Raises
-        ------
-        NotImplementedError
-            _description_
+        Parameters
+        ----------
+        trainset : Dataset
+            학습에 사용할 데이터셋
+        epochs : int
+            데이터셋을 반복할 횟수
+        batch_size : int
+            한 번 학습할 때 사용할 데이터의 크기
         """
-        raise NotImplementedError
+        self.train()
+        for _ in range(epochs):
+            train_loader = DataLoader(trainset, batch_size=batch_size)
+            self._train_one_epoch(train_loader)
 
-    def predict(self):
+    def predict(self, dataset: Dataset) -> torch.Tensor:
         """
-        _summary_
+        추천 결과를 생성합니다.
 
-        Raises
-        ------
-        NotImplementedError
-            _description_
-        """
-        raise NotImplementedError
+        Parameters
+        ----------
+        dataset : Dataset
+            추천에 사용할 데이터셋
 
-    def _train_one_epoch(self):
+        Returns
+        -------
+        torch.Tensor
+            추천 결과
         """
-        _summary_
+        self.eval()
+        pred = self(dataset.X)
 
-        Raises
-        ------
-        NotImplementedError
-            _description_
+        return pred
+
+    def _train_one_epoch(self, train_loader: DataLoader):
         """
-        raise NotImplementedError
+        데이터에 대해 1 epoch을 학습합니다.
+
+        Parameters
+        ----------
+        train_loader : DataLoader
+            에폭 안에서 사용할 데이터로더
+        """
+        for X, y_true in train_loader:
+            self._optimizer.zero_grad()
+            y_pred = self(X)
+            loss = self._loss(y_pred, y_true)
+            loss.backward()
+            self._optimizer.step()
 
     @property
-    def input_dim(self):
+    def input_dim(self) -> int:
         """
-        input_dim
+        AutoRec의 input dimension 값 출력
 
         Returns
         -------
         int
-            int
+           AutoRec의 input dimension 값
         """
         return self._input_dim
 
     @input_dim.setter
-    def input_dim(self, dim):
+    def input_dim(self, dim: int):
         """
-        _summary_
+        AutoRec의 input demension 값 변경
 
         Parameters
         ----------
         dim : _type_
-            _description_
+            새로운 input dimension
         """
         self._input_dim = dim
         self._encoder.in_features = dim
         self._decoder.out_features = dim
 
     @property
-    def hidden_dim(self):
+    def hidden_dim(self) -> int:
         """
-        _summary_
+        AutoRec의 hidden dimension 값 출력
 
         Returns
         -------
-        _type_
-            _description_
+        int
+            AutoRec의 hidden dimension 값
         """
         return self._hidden_dim
 
     @hidden_dim.setter
-    def hidden_dim(self, dim):
+    def hidden_dim(self, dim: int):
         """
-        _summary_
+        AutoRec의 hidden dimension 값 변경
 
         Parameters
         ----------
-        dim : _type_
-            _description_
+        dim : int
+            새로운 hidden dimension
         """
         self._hidden_dim = dim
         self._decoder.in_features = dim
         self._encoder.out_features = dim
 
     @property
-    def encoder(self):
+    def encoder(self) -> nn.Linear:
         """
-        _summary_
+        AutoRec의 encoder layer 부분 출력
 
         Returns
         -------
-        _type_
-            _description_
+        nn.Linear
+            AutoRec의 encoder layer
         """
         return self._encoder
 
     @property
-    def decoder(self):
+    def decoder(self) -> nn.Linear:
         """
-        _summary_
+        AutoRec의 decoder layer 부분 출력
 
         Returns
         -------
-        _type_
-            _description_
+        nn.Linear
+            AutoRec의 decoder layer
         """
         return self._decoder
 
     @property
-    def final_activation(self):
+    def final_activation(self) -> ACTIVATION:
         """
-        _summary_
+        AutoRec의 마지막 활성화 함수 출력
 
         Returns
         -------
-        _type_
-            _description_
+        ACTIVATION
+            AutoRec의 마지막 활성화 함수
         """
         return self._final_activation
 
     @final_activation.setter
-    def final_activation(self, activation):
+    def final_activation(self, activation: ACTIVATION):
         """
-        _summary_
+        AutoRec의 마지막 활성화 함수 변경
 
         Parameters
         ----------
-        activation : _type_
-            _description_
+        activation : ACTIVATION
+            새로운 마지막 활성화 함수
         """
         self._final_activation = activation
+
+    @property
+    def optimizer(self) -> Optimizer:
+        """
+        AutoRec의 optimizer 출력
+
+        Returns
+        -------
+        Optimizer
+            AutoRec의 optimizer
+        """
+        return self._optimizer
+
+    @optimizer.setter
+    def optimizer(self, optimizer: Union[str, Optimizer]):
+        """
+        AutoRec의 optimizer 변경
+
+        Parameters
+        ----------
+        optimizer : Union[str, Optimizer]
+            optimizer의 이름 혹은 실제 optimizer
+
+        Raises
+        ------
+        ValueError
+            str이나 Optimizer가 아닌 경우 에러
+        """
+        if isinstance(optimizer, str):
+            self._set_optimizer(optimizer)
+        elif isinstance(optimizer, Optimizer):
+            self._optimizer = optimizer
+        else:
+            raise ValueError
+
+    @property
+    def loss(self) -> _Loss:
+        """
+        AutoRec의 손실함수 출력
+
+        Returns
+        -------
+        _Loss
+            AutoRec의 손실함수
+        """
+        return self._loss
+
+    @loss.setter
+    def loss(self, loss: _Loss):
+        """
+        AutoRec의 손실함수 변경
+
+        Parameters
+        ----------
+        loss : _Loss
+            새로운 손실함수
+        """
+        self._loss = loss
